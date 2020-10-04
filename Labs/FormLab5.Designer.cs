3191f95a10b61687523dcbb4ecc2194ce11b025e
Tyoma Kukharuk <kukharuk.artem@gmail.com>
2020-10-05 01:00:42 +0300

    5 лабараторна

---
+++ Labs/FormLab5.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Labs
+{
+    partial class FormLab5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(495, 261);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.groupBox2.Location = new System.Drawing.Point(254, 10);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(230, 70);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Пальто";
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.Control;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(7, 40);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(217, 23);
+            this.button9.TabIndex = 2;
+            this.button9.Text = "Створити";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.jacket_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.radioButton3.Location = new System.Drawing.Point(122, 17);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(101, 17);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.Text = "З параметрами";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.radioButton4.Location = new System.Drawing.Point(7, 17);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(110, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "По замовчуванню";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 70);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Куртка";
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.Control;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(7, 40);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(217, 23);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "Створити";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.blouse_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.radioButton2.Location = new System.Drawing.Point(122, 17);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(101, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "З параметрами";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.radioButton1.Location = new System.Drawing.Point(7, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(110, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "По замовчуванню";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.button7.Location = new System.Drawing.Point(3, 208);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(489, 50);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Завершити роботу";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.button6.Location = new System.Drawing.Point(35, 139);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(424, 50);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Знайти карликове пальто потрібного розміру";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.button5.Location = new System.Drawing.Point(35, 83);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(424, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Тепла куртка\r\nпо замовчуванню";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // FormLab5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 261);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormLab5";
+            this.Opacity = 0.95D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабараторна №5";
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
---
+++ Labs/FormLab5.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labs
+{
+    public partial class FormLab5 : Form
+    {
+        public FormLab5()
+        {
+            InitializeComponent();
+        }
+        private void blouse_Click(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                String name_cons, color_cons; int size_cons;
+                name_cons = Convert.ToString(Interaction.InputBox("Введіть назву для куртки\n", "Створюємо Куртку", "LeeCo"));
+                color_cons = Convert.ToString(Interaction.InputBox("Введіть колір", name_cons, "Сірий"));
+                size_cons = Convert.ToInt32(Interaction.InputBox("Введіть розмір\n", name_cons, "38"));
+                new Blouse_lab5(name_cons, color_cons, size_cons).Info();
+            }
+            else
+            {
+                Blouse_lab5 b1 = new Blouse_lab5("Adidas", "Зелена", 36);
+                b1.Info();
+                b1.Size = -5;
+                MessageBox.Show("Дані куртки після зміни розміру на -5");
+                b1.Info();
+                b1.Size = 46;
+                MessageBox.Show("Дані куртки після зміни розміру на 46");
+                b1.Info();
+            }
+        }
+        private void jacket_Click(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                String name_cons, color_cons; int size_cons;
+                name_cons = Convert.ToString(Interaction.InputBox("Введіть назву для пальта\n", "Створюємо Пальто", "Crocco"));
+                color_cons = Convert.ToString(Interaction.InputBox("Введіть колір", name_cons, "Оранжевий"));
+                size_cons = Convert.ToInt32(Interaction.InputBox("Введіть розмір\n", name_cons, "32"));
+                new Jacket_lab5(name_cons, color_cons, size_cons).Info();
+            }
+            else
+            {
+                Jacket_lab5 j1 = new Jacket_lab5("Puma", "Чорний", 34);
+                j1.Info();
+                j1.Size = -5;
+                MessageBox.Show("Дані пальта після зміни розміру на -5");
+                j1.Info();
+                j1.Size = 39;
+                MessageBox.Show("Дані пальта після зміни розміру на 39");
+                j1.Info();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            HotBlouse_lab5 j1 = new HotBlouse_lab5("Fabric", "Білий", 44);
+            j1.Info();
+            j1.Size = -5;
+            MessageBox.Show("Дані теплої куртки після зміни розміру на -5");
+            j1.Info();
+            j1.Size = 40;
+            MessageBox.Show("Дані теплої куртки після зміни розміру на 40");
+            j1.Info();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var sjsh = new StyleJacket_lab5("Carlic", "Жовтий", 50);
+            int i = 1;
+            int maxSize = 10;
+            maxSize = Convert.ToInt32(Interaction.InputBox("Введіть обмеження розміру для куртки\n(Меньше стандартного розміру \"36\")", "Карликове стильне пальто", "15"));
+            while (sjsh[i].Size > maxSize) i++;
+            MessageBox.Show("Стильне карликове пальто № " + i + " має розмір: " + sjsh[i].Size, "Стильне карликове пальто", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+    public abstract class Outerwear_lab5
+    {
+        public static int count;
+        public int nomer;
+        public String name, color, status;
+        public bool isFreak;
+        public Outerwear_lab5()
+        {
+            count++;
+            nomer = count;
+        }
+        ~Outerwear_lab5()
+        {
+          /*  MessageBox.Show("Знищується Верхній Одяг №" + nomer,
+                "Увага",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Exclamation); */
+            count--;
+            //FormLab5.asd.Text = "Кількість об'єктів: " + count;
+        }
+        virtual public void Info()
+        {
+           /* MessageBox.Show("Верхній одяг №" + nomer + " створено",
+                "Інформація",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information); */
+        }
+    }      
+    public class HotBlouse_lab5 : Blouse_lab5
+    {
+        public bool isHot;
+        public HotBlouse_lab5(String name, String color, int size) : base(name, color, size)
+        {
+            isHot = true;
+        }
+        public override void Info()
+        {
+            MessageBox.Show("Тепла куртка бренду: " + name + "\n" + color + " колiр\nРозмір: " + Size + "\n" + isFreaks() + "\n\n" + SizeString() + "\n\n\n",
+                "Ось такa тепла куртка",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+    public class Blouse_lab5 : Outerwear_lab5
+    {
+        private int size;
+        public int Size { get { return size; } set { if (value >= 0) size = value; } }
+        public Blouse_lab5(String name, String color, int size) : base()
+        {
+            base.Info();
+            this.name = name; this.color = color; this.size = size; this.status = "Новa";
+            Random rnd = new Random(); int asd = rnd.Next(0, 2); if (asd == 0) isFreak = false; else isFreak = true;
+        }
+        public string isFreaks()
+        {
+            if (isFreak == true)
+                if (status.Equals("Потаскана"))
+                    return "Жахлива куртка";
+                else return "Досить мила куртка...";
+            else return "Класна куртка :р";
+        }
+        public string SizeString()
+        {
+            if (size > 25)
+                if (size < 55)
+                    return "Ця куртка навіть могла б підійти для тебе";
+                else return "Ця куртка завелика для тебе!";
+            else return "Ця куртка замала для тебе!";
+        }
+        public override void Info()
+        {
+            MessageBox.Show("Куртка бренду: " + name + "\n" + color + " колiр\nРозмір: " + size + "\n" + isFreaks() + "\n\n" + SizeString() + "\n\n\n",
+                "Ось такa куртка",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+    public class StyleJacket_lab5 : Jacket_lab5
+    {
+        public StyleJacket_lab5(String name, String color, int size) : base(name, color, size)
+        { }
+        public StyleJacket_lab5 this[int i]
+        { get
+            {
+                if (i <= 0)
+                    return new StyleJacket_lab5(name, color, Size);
+                else
+                    return new StyleJacket_lab5(name, color, (Size - i));
+            }
+        }
+    }
+    public class Jacket_lab5 : Outerwear_lab5
+    {
+        private int size;
+        public int Size { get { return size; } set { if (value >= 0) size = value; } }
+        public Jacket_lab5(String name, String color, int size) : base()
+        {
+            base.Info();
+            this.name = name; this.color = color; this.size = size; this.status = "Новa";
+            Random rnd = new Random(); int asd = rnd.Next(0, 2); if (asd == 0) isFreak = false; else isFreak = true;
+        }
+        public string isFreaks()
+        {
+            if (isFreak == true)
+                if (status.Equals("Потаскане"))
+                    return "Жахливе пальто";
+                else return "Досить миле пальто...";
+            else return "Класне пальто :р";
+        }
+        public string SizeString()
+        {
+            if (size > 25)
+                if (size < 55)
+                    return "Це пальто навіть могло б підійти для тебе";
+                else return "Це пальто завелике для тебе!";
+            else return "Це пальто замале для тебе!";
+        }
+        public override void Info()
+        {
+            MessageBox.Show("Пальто бренду: " + name + "\n" + color + " колiр\nРозмір: " + size + "\n" + isFreaks() + "\n\n" + SizeString() + "\n\n\n",
+                "Ось таке Пальто",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+
+}
