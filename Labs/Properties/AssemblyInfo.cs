5db049e06f3a7c3fd788fff48be943b26910ad6b
tyomashi <kukharuk.artem@gmail.com>
2020-10-15 14:08:18 +0300

    update version

--- Labs/Properties/AssemblyInfo.cs
+++ Labs/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.3.5")]
-[assembly: AssemblyFileVersion("2.5.3.5")]
+[assembly: AssemblyVersion("2.5.6.12")]
+[assembly: AssemblyFileVersion("2.5.6.12")]
