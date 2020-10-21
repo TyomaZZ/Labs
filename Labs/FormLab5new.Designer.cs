8ba651a51f70d2ec3e5020e1ba1f56a293c55903
tyomashi <kukharuk.artem@gmail.com>
2020-10-22 02:19:39 +0300

    Я хз шо я там міняв

--- Labs/FormLab5new.Designer.cs
+++ Labs/FormLab5new.Designer.cs
@@ -204,7 +204,7 @@
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FormLab5new";
+            this.Text = "Лабараторна №5";
             this.ResumeLayout(false);
 
         }
