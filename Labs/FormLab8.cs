65cf39d2abcc75961c7ece454d677ef82e052b2b
Tyoma Kukharuk <kukharuk.artem@gmail.com>
2020-11-11 15:49:49 +0200

    8 лаба

---
+++ Labs/FormLab8.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labs
+{
+    public partial class FormLab8 : Form
+    {
+        public FormLab8()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            string cell = "к";
+            string s = Interaction.InputBox("Введіть слово, словосполучення або речення для взаємодії", "Введення", "Приклад");
+            //result = s.Replace("к", ""); result = result.Replace("К", "");
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!String.Equals(s[i].ToString(), cell, StringComparison.OrdinalIgnoreCase)) {
+                    result += s[i];
+                }
+            }
+            MessageBox.Show("Стартова строка: " + s + "\nРезультат дії: " + result, "Результат");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            string cell = "?";
+            string s = Interaction.InputBox("Введіть слово, словосполучення або речення для взаємодії", "Введення", "Приклад?");
+            //result = s.Replace("к", ""); result = result.Replace("К", "");
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += s[i];
+                if (!String.Equals(s[i].ToString(), cell))
+                {
+                    result += s[i];
+                }
+            }
+            MessageBox.Show("Стартова строка: " + s + "\nРезультат дії: " + result, "Результат");
+        }
+    }
+}
